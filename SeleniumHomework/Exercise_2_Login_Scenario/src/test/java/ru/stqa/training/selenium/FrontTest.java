58118e86edeb80b6216366bbfdc67313b0ba663c
Степанов Виталий <escapefox301@gmail.com>
2021-11-12 19:05:33 +0600

    Исправление Задания 2

--- SeleniumHomework/Exercise_2_Login_Scenario/src/test/java/ru/stqa/training/selenium/FrontTest.java
+++ SeleniumHomework/Exercise_2_Login_Scenario/src/test/java/ru/stqa/training/selenium/FrontTest.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 public class FrontTest extends TestBase {
+
     @Test
     public void loginLitecart(){
         driver.get(props.getProperty("baseUrl"));
         driver.findElement(By.name("username")).sendKeys(props.getProperty("login"));
-        driver.findElement(By.name("password")).sendKeys(props.getProperty("login"));
+        driver.findElement(By.name("password")).sendKeys(props.getProperty("password"));
         driver.findElement(By.name("login")).click();
-
     }
 }
